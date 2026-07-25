76aedc4f6f7258b2444614bfb82ad69fd9f5ed5a
fauzirahman <fauzirahman45@yahoo.com>
2026-07-25 11:51:49 +0700

    feat: setup prisma orm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { HealthModule } from './modules/health/health.module';
+import { PrismaModule } from './database/prisma.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
-      envFilePath: '.env',
     }),
-    HealthModule,
+    PrismaModule,
   ],
 })
 export class AppModule {}
