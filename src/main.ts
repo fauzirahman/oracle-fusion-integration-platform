92d2f4f524a85c97e1b2cf8228486b6cf9ffefd7
fauzirahman <fauzirahman45@yahoo.com>
2026-08-14 22:57:59 +0700

    fix: enable cors for integration dashboard

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,26 @@ import { HttpExceptionFilter } from './common/filters/http-exception.filter';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  // Allow the React/Next.js dashboard to access the API.
+  const allowedOrigins = [
+    'http://localhost:3000',
+    process.env.FRONTEND_URL,
+  ].filter((origin): origin is string => Boolean(origin));
+
+  app.enableCors({
+    origin: allowedOrigins,
+    methods: [
+      'GET',
+      'HEAD',
+      'PUT',
+      'PATCH',
+      'POST',
+      'DELETE',
+      'OPTIONS',
+    ],
+    credentials: true,
+  });
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
